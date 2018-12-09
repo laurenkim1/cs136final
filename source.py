f5fa6c9adbc7daa31c45875606b04b6475f1fac3
janetchen1 <janetchen@college.harvard.edu>
2018-12-08 21:05:24 -0500

    bug fixed, added logging

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 from graph import *
 import operator
 import random
+import sys
 
 # model skills using integers (money will be modeled using negative values)
 NUM_SKILLS = 10
@@ -34,7 +35,7 @@ class Agent:
 
 	# add skill to this agent's portfolio
 	def add_skill(self, skill):
-		if skill <= NUM_SKILLS and skill >= 0:
+		if skill <= NUM_SKILLS:
 			if skill not in self.skills:
 				# (rating, number of ratings)
 				self.skills[skill] = (5.0, 1)
@@ -158,13 +159,11 @@ def getAgents(G, cycle, agents):
    if cycle.vertexId in agents:
       cycle = cycle.anyNext()
 
-   startingHouse = cycle
-   currentVertex = startingHouse.anyNext()
+   currentVertex = cycle
    theAgents = set()
 
    while currentVertex not in theAgents:
       theAgents.add(currentVertex)
-      currentVertex = currentVertex.anyNext()
       currentVertex = currentVertex.anyNext()
 
    return theAgents
@@ -188,6 +187,7 @@ def anyCycle(G):
 # initiailOwnerships is a dict {houses:agents}
 def topTradingCycles(agents, houses, agentPreferences, initialOwnership):
    # form the initial graph
+   average_pref_granted = 0
    agents = set(agents)
    vertexSet = set(agents)
    G = Graph(vertexSet)
@@ -201,7 +201,7 @@ def topTradingCycles(agents, houses, agentPreferences, initialOwnership):
 
    for a in agents:
     # add an edge to agent's most desired peer to complete task
-   	G.addEdge(a, preferredAgent(a))
+    G.addEdge(a, preferredAgent(a))
 
    # iteratively remove top trading cycles
    allocation = dict()
@@ -212,11 +212,14 @@ def topTradingCycles(agents, houses, agentPreferences, initialOwnership):
 
       # assign agents in the cycle their house
       for a in cycleAgents:
+      	 average_pref_granted += currentPreferenceIndex[a.vertexId]
          assigned_agent = a.anyNext().vertexId
          allocation[a.vertexId] = assigned_agent
          if a.vertexId != assigned_agent:
          	num_exchanges += 1
-         G.delete(a)
+         
+      for a in cycleAgents:
+      	G.delete(a)
 
       for a in agents:
          if a in G.vertices and G[a].outdegree() == 0:
@@ -224,11 +227,13 @@ def topTradingCycles(agents, houses, agentPreferences, initialOwnership):
                currentPreferenceIndex[a] += 1
             G.addEdge(a, preferredAgent(a))
 
-   print "exchanges: ", num_exchanges
-   return (num_exchanges, allocation)
+   return (num_exchanges, average_pref_granted / len(agents), allocation)
 
+########################
+# Back to our own code #
+########################
 
-def main():
+def one_trial():
 	# initialize market
 	mkt = MarketPlace()
 
@@ -237,37 +242,59 @@ def main():
 	all_agents = []
 	for i in range(NUM_AGENTS):
 		new_agent = Agent(i)
-		# give each agent SKILLS_PER_AGENT random service skills
-		for j in range(SKILLS_PER_AGENT):
-			new_skill = random.randint(1,NUM_SKILLS)
-			new_agent.add_skill(new_skill)
+		total = 0
 		# randomly assign some agents money as a skill
 		if random.random() < AGENTS_WITH_MONEY:
 			new_skill = -1 * random.randint(1,MAX_MONEY)
 			new_agent.add_skill(new_skill)
+			total = 1
+		# give each agent SKILLS_PER_AGENT random service skills
+		for j in range(SKILLS_PER_AGENT - total):
+			new_skill = random.randint(1,NUM_SKILLS)
+			new_agent.add_skill(new_skill)
+		
 		all_agents.append(new_agent)
 
 		# register agent in the market
 		mkt.register_agent(new_agent)
-		# randomly pick a want from all skills that are not agent's skills
+
+		# pick a skill for the agent to offer
+		offer = new_agent.offer_skill()
+
+		# randomly pick a want from all skills (and money) that are not agent's skills
 		agent_skills = new_agent.get_skills()
 
 		want = agent_skills[0]
 		while want in agent_skills:
-			# some proportion of agents want money
-			if random.random() < AGENTS_WANT_MONEY:
-				want = -1 * random.randint(1, MAX_MONEY)
-			# other agents want a skill that they don't have
-			else:
-				want = random.randint(1, NUM_SKILLS)
+			# agents want a skill that they don't have
+			want = random.randint(1, NUM_SKILLS)
 
-		# pick a skill for the agent to offer
-		offer = new_agent.offer_skill()
+			# some proportion of agents want money, unless their offer is money
+			if want > 0:
+				if random.random() < AGENTS_WANT_MONEY:
+					want = -1 * random.randint(1, MAX_MONEY)
+	
 		mkt.enter_market(new_agent, want, offer)
-		print i, ' ', want, ' ', offer
 
 	# once all agents registered, run TTC
-	mkt.make_match()
+	return mkt.make_match()
+
+
+
+
+def main():
+	num_iterations = int(sys.argv[1])
+	avg_exchanges = 0
+	avg_pref_overall = 0
+	for i in range(num_iterations):
+		(exchanges, avg_pref, alloc) = one_trial()
+		avg_exchanges += exchanges
+		avg_pref_overall += avg_pref
+	avg_exchanges = avg_exchanges / num_iterations
+	avg_pref_overall = avg_pref_overall / num_iterations
+
+	print "On average, ", avg_exchanges, " / ", NUM_AGENTS, " were included"
+	print "On average, user got ", avg_pref_overall, "th preference overall (zero-indexed)"
 
 
 def test_1():
@@ -287,15 +314,56 @@ def test_1():
 	mkt.enter_market(agent_2, 1, 2)
 
 	print "Agent 1 wants 2, offers 1. Agent 2 wants 1, offers 2."
-	print "Expecting 1 exchange..."
+	print "Expecting 2 exchanged agents..."
 
 	# once all agents registered, run TTC
-	(num_exchanges, alloc) = mkt.make_match()
+	(num_exchanges, avg_pref, alloc) = mkt.make_match()
 
-	if not num_exchanges == 1:
+	print "average pref granted: ", avg_pref
+	print alloc
+
+	if not num_exchanges == 2:
 		raise AssertionError()
 
-test_1()
+def test_2():
+	# initialize market
+	mkt = MarketPlace()
+
+	# initialize 2 agents
+	agent_1 = Agent(0)
+	agent_1.add_skill(1)
+	mkt.register_agent(agent_1)
+
+	agent_2 = Agent(1)
+	agent_2.add_skill(2)
+	mkt.register_agent(agent_2)
+
+	agent_3 = Agent(2)
+	agent_3.add_skill(3)
+	mkt.register_agent(agent_3)
+
+	agent_4 = Agent(3)
+	agent_4.add_skill(4)
+	mkt.register_agent(agent_4)
+
+	mkt.enter_market(agent_1, 2, 1)
+	mkt.enter_market(agent_2, 3, 2)
+	mkt.enter_market(agent_3, 4, 3)
+	mkt.enter_market(agent_4, 1, 4)
+
+	print "Expecting 4 exchanged agents..."
+
+	# once all agents registered, run TTC
+	(num_exchanges, avg_pref, alloc) = mkt.make_match()
+
+	print "average pref granted: ", avg_pref
+	print alloc
+
+	if not num_exchanges == 4:
+		raise AssertionError()
+
+#test_1()
+#test_2()
 main()
 
 
